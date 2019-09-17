621e6aefca10b7b06e6293cdb9e02f0aa109ca2f
ivanpiskun123 <ivan.piskun.s@gmail.com>
2019-09-18 02:08:21 +0300

    Check path omniauth

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -3,6 +3,15 @@ class CampaignsController < ApplicationController
   before_action :find_campaign, only: [:edit,:show,:update, :destroy]
 
   def index
+    puts ''
+    puts ''
+    puts ''
+    puts ''
+    print "7777777777777777777777777777777777777777777777777777777"
+    puts ''
+    puts ''
+    puts ''
+    puts ''
     @campaigns = Campaign.all.order("created_at DESC")
   end
 
